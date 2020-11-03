1f502a8102b31053be4bfe48bad167df9e1f4cb4
Ishaan Shah <ishaan.n.shah@gmail.com>
2020-11-03 10:14:24 +0530

    Add test_oracle function

--- Code/src/simons/simons_oracle.py
+++ Code/src/simons/simons_oracle.py
@@ -46,3 +46,48 @@ def simons_oracle(size: int) -> Tuple[Gate, List[int]]:
             oracle.x(output[i])
 
     return oracle.to_gate(label="oracle"), secret_input
+
+
+def test_oracle():
+    """ Tests the oracle against all possible inputs of length 4
+        to verify its working as expected """
+    # Get backend
+    backend = Aer.get_backend('qasm_simulator')
+
+    # Length of input
+    n = 4
+
+    # Initialise input, output and secret registers
+    input = QuantumRegister(n, "input")
+    output = QuantumRegister(n, "output")
+    secret = QuantumRegister(n, "secret")
+    result = ClassicalRegister(n)
+
+    (oracle, secret_input) = simons_oracle(n)
+    print(f"Secret string - {''.join([str(bit) for bit in secret_input])}")
+
+    # Test Oracle
+    for seq in itertools.product(["0", "1"], repeat=n):
+        # Initialize circuit
+        circuit = QuantumCircuit(input, output, secret, result)
+
+        # Inititialise input qubits
+        desired_vector = [0 for i in range(2**n)]
+        desired_vector[int("".join(seq), 2)] = 1
+        circuit.initialize(desired_vector, input)
+
+        circuit.append(oracle, [*input, *output, *secret])
+
+        # Perform measurement
+        circuit.measure(output, result)
+
+        inp = list(seq)
+        inp.reverse()
+        print(
+            f"{''.join(inp)} - "
+            f"{list(execute(circuit, backend).result().get_counts(circuit))[0]}"
+        )
+
+
+if __name__ == "__main__":
+    test_oracle()
