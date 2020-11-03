bb8931e856add5edbadd992f01681653918e714d
Ishaan Shah <ishaan.n.shah@gmail.com>
2020-11-03 09:58:08 +0530

    Switch to gate based oracle

--- Code/src/simons/simons_algorithm.py
+++ Code/src/simons/simons_algorithm.py
@@ -1,6 +1,4 @@
 import itertools
-import random
-import time
 
 from qiskit import (Aer, ClassicalRegister,
                     QuantumCircuit, QuantumRegister,
@@ -9,7 +7,6 @@ from qiskit.visualization import plot_histogram
 
 from simons_oracle import simons_oracle
 
-random.seed(time.monotonic())
 
 # Get backend
 backend = Aer.get_backend('qasm_simulator')
@@ -17,33 +14,27 @@ backend = Aer.get_backend('qasm_simulator')
 # Length of input
 n = 4
 
-# Initialise input and output registers
+# Initialise input, output and secret registers
 input = QuantumRegister(n, "input")
 output = QuantumRegister(n, "output")
+secret = QuantumRegister(n, "secret")
 result = ClassicalRegister(n)
 
-# Initialise circuit
-circuit = QuantumCircuit(input, output, result)
-
-# Generate random secret string
-secret_input = [0 for i in range(n)]
-while secret_input.count(0) == n:
-    secret_input = [random.choice([0, 1]) for i in range(n)]
+(oracle, secret_input) = simons_oracle(n)
 print(f"Secret string - {''.join([str(bit) for bit in secret_input])}")
 
 # Test Oracle
 for seq in itertools.product(["0", "1"], repeat=n):
-    # Copy circuit
-    circ = circuit.copy()
+    # Initialize circuit
+    circuit = QuantumCircuit(input, output, secret, result)
 
     # Inititialise input qubits
     desired_vector = [0 for i in range(2**n)]
     desired_vector[int("".join(seq), 2)] = 1
-    circ.initialize(desired_vector, input)
+    circuit.initialize(desired_vector, input)
 
-    # Apply simons oracle circuit
-    simons_oracle(circ, input, output, secret_input)
+    circuit.append(oracle, [*input, *output, *secret])
 
     # Perform measurement
-    circ.measure(output, result)
-    print(f"{seq} - {execute(circ, backend).result().get_counts(circ)}")
+    circuit.measure(output, result)
+    print(f"{seq} - {execute(circuit, backend).result().get_counts(circuit)}")
--- Code/src/simons/simons_oracle.py
+++ Code/src/simons/simons_oracle.py
@@ -1,45 +1,48 @@
-from typing import List
+import random
+import time
+from typing import List, Tuple
 
 from qiskit import QuantumCircuit, QuantumRegister
+from qiskit.circuit import Gate
+
+random.seed(time.monotonic())
 
 
-def simons_oracle(circuit: QuantumCircuit,
-                  input: QuantumRegister,
-                  output: QuantumRegister,
-                  secret_input: List[int]) -> QuantumRegister:
-    # Size of secret, input and output
-    (s, n) = (input.size, input.size)
+def simons_oracle(size: int) -> Tuple[Gate, List[int]]:
     # Register holding the secret string
-    secret = QuantumRegister(s, "secret")
-    circuit.add_register(secret)
+    secret = QuantumRegister(size, "secret")
+
+    # Input and Output registers
+    input = QuantumRegister(size, "input")
+    output = QuantumRegister(size, "output")
+
+    # Initialize circuit
+    oracle = QuantumCircuit(input, output, secret)
+
+    # Generate random secret string
+    secret_input = [0 for i in range(size)]
+    while secret_input.count(0) == size:
+        secret_input = [random.choice([0, 1]) for i in range(size)]
 
     # Encode it in the quantum register
-    for i in range(s):
+    for i in range(size):
         if secret_input[i]:
-            circuit.x(secret[i])
-
-    circuit.barrier()
+            oracle.x(secret[i])
 
     # Copy input register to output register
-    for i in range(n):
-        circuit.cx(input[i], output[i])
-
-    circuit.barrier()
+    for i in range(size):
+        oracle.cx(input[i], output[i])
 
     # Find msb of secret string
     msb = secret_input.index(1)
 
     # Create 2-1 mapping
-    for i in range(n):
-        circuit.ccx(input[msb], secret[i], output[i])
-
-    circuit.barrier()
+    for i in range(size):
+        oracle.ccx(input[msb], secret[i], output[i])
 
     # Randomly flip qubits for further obfuscation
-    for i in range(n):
+    for i in range(size):
         if i % 3 == 0:
-            circuit.x(output[i])
+            oracle.x(output[i])
 
-    circuit.barrier()
-
-    return secret
+    return oracle.to_gate(label="oracle"), secret_input
