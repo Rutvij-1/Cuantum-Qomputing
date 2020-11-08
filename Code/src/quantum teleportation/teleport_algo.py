eb2f8a2489c3e13ec31765b7233a3ba99715c9a0
Rutvij Menavlikar <menavlikarrutvij@gmail.com>
2020-11-08 21:40:28 +0530

    Updated Teleportation algorithm by using a custome made 3-qubit gate

--- Code/src/quantum teleportation/teleport_algo.py
+++ Code/src/quantum teleportation/teleport_algo.py
@@ -6,21 +6,30 @@ from qiskit_textbook.tools import random_state, array_to_latex
 from matplotlib import *
 
 
-def quantum_teleport(qc, input_reg, output_reg):
+def teleport_gate():
+    inp = QuantumRegister(1)
+    inter = QuantumRegister(1)
+    out = QuantumRegister(1)
+    circuit = QuantumCircuit(inp, inter, out)
+    circuit.h(inter)
+    circuit.cx(inter, out)
+    circuit.cx(inp, inter)
+    circuit.h(inp)
+    return circuit.to_gate(label="Entangle for teleport")
+
+
+def quantum_teleport(qc, input_reg, output_reg, intermediate_reg=None):
     # Add the needed registers
     crz = ClassicalRegister(1, name='crz')
     crx = ClassicalRegister(1, name='crx')
-    intermediate_reg = QuantumRegister(1, name='intermediate register')
-    qc.add_register(intermediate_reg)
+    if intermediate_reg is None:
+        intermediate_reg = QuantumRegister(1, name='intermediate register')
+        qc.add_register(intermediate_reg)
     qc.add_register(crz)
     qc.add_register(crx)
 
     # Proceed to teleportation.
-    qc.h(intermediate_reg)
-    qc.cx(intermediate_reg, output_reg)
-    qc.barrier()
-    qc.cx(input_reg, intermediate_reg)
-    qc.h(input_reg)
+    qc.append(teleport_gate(), [input_reg, intermediate_reg, output_reg])
     qc.barrier()
     qc.measure(input_reg, crz)
     qc.measure(intermediate_reg, crx)
